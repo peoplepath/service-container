cacfa13d633044724f2b43daf94ce793a6345942
Ondřej Ešler <ondrej.esler@intraworlds.com>
2018-10-31 23:23:02 +0100

    add more specs

--- spec/IW/ServiceContainerSpec.php
+++ spec/IW/ServiceContainerSpec.php
@@ -7,43 +7,74 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Psr\Container\ContainerInterface;
 use IW\ServiceContainer\CannotAutowireInterfaceException;
+use IW\ServiceContainer\EmptyResultFromFactoryException;
 use IW\ServiceContainer\ServiceNotFoundException;
 
 class ServiceContainerSpec extends ObjectBehavior
 {
-    function it_is_initializable(): void
+    function it_is_initializable()
     {
         $this->shouldHaveType(ServiceContainer::class);
     }
 
-    function it_implements_psr11(): void
+    function it_implements_psr11()
     {
         $this->shouldImplement(ContainerInterface::class);
     }
 
-    function it_returns_any_initializable(): void
+    function it_returns_any_initializable_with_disabled_autowire()
     {
+        $this->beConstructedWith(false);
         $this->get('DateTime')->shouldBeAnInstanceOf('DateTime');
     }
 
-    function it_knows_any_initializable(): void
+    function it_knows_any_initializable_with_disabled_autowire()
     {
+        $this->beConstructedWith(false);
         $this->has('DateTimeImmutable')->shouldReturn(true);
     }
 
-    function it_fail_autowiring_for_interface(): void
+    function it_fail_autowiring_for_interface()
     {
         $this->shouldThrow(CannotAutowireInterfaceException::class)->duringGet('DateTimeInterface');
     }
 
-    function it_fail_for_non_existing_class(): void
+    function it_fail_for_non_existing_class()
     {
         $this->shouldThrow(ServiceNotFoundException::class)->duringGet('NoClassHere');
     }
 
-    function it_autowire_any_dependency_tree(): void
+    function it_autowire_any_dependency_tree()
     {
         $this->get(Baz::class)->shouldBeAnInstanceOf(Baz::class);
+    }
+
+    function it_creates_singletons_by_default()
+    {
+        $bar = $this->getWrappedObject()->get(Bar::class);
+        $this->get(Bar::class)->shouldBe($bar);
+    }
+
+    function it_generates_fresh_instances_when_singleton_is_disabled()
+    {
+        $this->setSingleton(Bar::class, false);
+        $bar = $this->getWrappedObject()->get(Bar::class);
+        $this->get(Bar::class)->shouldNotBe($bar);
+    }
+
+    function it_accept_non_class_id_if_factory_is_defined()
+    {
+        $this->setFactory('logger', function () {
+            return new Foo;
+        });
+
+        $this->get('logger')->shouldBeAnInstanceOf(Foo::class);
+    }
+
+    function it_fail_when_factory_result_is_empty() {
+        $this->setFactory('nothing', function () {});
+
+        $this->shouldThrow(EmptyResultFromFactoryException::class)->duringGet('nothing');
     }
 
 }
@@ -57,3 +88,4 @@ class Bar {
 class Baz {
     function __construct(Bar $bar, Foo $foo) {}
 }
+
