411c0ec2ac3a0549d35f792f8e4870a19ab54c2e
Ondřej Ešler <ondrej.esler@intraworlds.com>
2018-10-31 20:05:53 +0100

    add more specs

--- spec/IW/ServiceContainerSpec.php
+++ spec/IW/ServiceContainerSpec.php
@@ -6,6 +6,8 @@ use IW\ServiceContainer;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Psr\Container\ContainerInterface;
+use IW\ServiceContainer\CannotAutowireInterfaceException;
+use IW\ServiceContainer\ServiceNotFoundException;
 
 class ServiceContainerSpec extends ObjectBehavior
 {
@@ -26,6 +28,32 @@ class ServiceContainerSpec extends ObjectBehavior
 
     function it_knows_any_initializable(): void
     {
-        $this->has('DateTime')->shouldReturn(true);
+        $this->has('DateTimeImmutable')->shouldReturn(true);
     }
+
+    function it_fail_autowiring_for_interface(): void
+    {
+        $this->shouldThrow(CannotAutowireInterfaceException::class)->duringGet('DateTimeInterface');
+    }
+
+    function it_fail_for_non_existing_class(): void
+    {
+        $this->shouldThrow(ServiceNotFoundException::class)->duringGet('NoClassHere');
+    }
+
+    function it_autowire_any_dependency_tree(): void
+    {
+        $this->get(Baz::class)->shouldBeAnInstanceOf(Baz::class);
+    }
+
+}
+
+class Foo {}
+
+class Bar {
+    function __construct(Foo $foo) {}
+}
+
+class Baz {
+    function __construct(Bar $bar, Foo $foo) {}
 }
