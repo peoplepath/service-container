e21d68249e8193e3b573cd213719c6131c2cbcfc
Ondřej Ešler <ondrej.esler@intraworlds.com>
2018-10-31 16:16:06 +0100

    spec whole PSR-11

--- spec/IW/ServiceContainerSpec.php
+++ spec/IW/ServiceContainerSpec.php
@@ -5,6 +5,7 @@ namespace spec\IW;
 use IW\ServiceContainer;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Psr\Container\ContainerInterface;
 
 class ServiceContainerSpec extends ObjectBehavior
 {
@@ -13,8 +14,18 @@ class ServiceContainerSpec extends ObjectBehavior
         $this->shouldHaveType(ServiceContainer::class);
     }
 
+    function it_implements_psr11(): void
+    {
+        $this->shouldImplement(ContainerInterface::class);
+    }
+
     function it_returns_any_initializable(): void
     {
         $this->get('DateTime')->shouldBeAnInstanceOf('DateTime');
+    }
+
+    function it_knows_any_initializable(): void
+    {
+        $this->has('DateTime')->shouldReturn(true);
     }
 }
