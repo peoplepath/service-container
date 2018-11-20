21ef635b2cc6eae028f8c24524d12a3d7b5682d7
Ondřej Ešler <ondrej.esler@intraworlds.com>
2018-11-20 16:26:40 +0100

    main class can catch Throwable, fix it in the main Exception

--- src/ServiceContainer/Exception.php
+++ src/ServiceContainer/Exception.php
@@ -7,7 +7,7 @@ use Psr\Container\ContainerExceptionInterface;
 class Exception extends \Exception implements ContainerExceptionInterface
 {
 
-    public function __construct(string $message = null, \Exception $previous = null) {
+    public function __construct(string $message = null, \Throwable $previous = null) {
         parent::__construct($message, 0, $previous);
     }
 
