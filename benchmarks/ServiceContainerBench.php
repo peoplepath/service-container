55415d07565a27f9575de414533f91e040516497
Ondřej Ešler <ondrej.esler@intraworlds.com>
2020-07-10 09:31:34 +0200

    dynamic classes for benchmark
    will measure building factories too

--- benchmarks/ServiceContainerBench.php
+++ benchmarks/ServiceContainerBench.php
@@ -4,53 +4,52 @@ declare(strict_types=1);
 
 namespace IW;
 
-use IW\Fix;
+use function array_pop;
+use function sprintf;
 
+/**
+ * @BeforeMethods({"init"})
+ */
 class ServiceContainerBench
 {
     /** @var ServiceContainer */
     private $container;
+
+    /** @var string[] */
+    private $classnames = [];
 
     public function __construct()
     {
         $this->container = new ServiceContainer();
     }
 
+    public function init(): void
+    {
+        for ($i = 0; $i < 1000; $i++) {
+            $classname = 'benchclass' . $i;
+
+            eval(sprintf('class %s {}', $classname));
+
+            $this->classnames[] = $classname;
+        }
+    }
+
     /**
      * @Revs(1000)
      * @Iterations(5)
      */
-    public function benchMake()
+    public function benchGet(): void
     {
         $container = new ServiceContainer();
-        $container->make(Fix\First::class);
+        $container->get((string) array_pop($this->classnames));
     }
 
     /**
      * @Revs(1000)
      * @Iterations(5)
      */
-    public function benchMakeWithSingleton()
+    public function benchGetSingletons(): void
     {
-        $this->container->make(Fix\First::class);
-    }
-
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
-    public function benchGet()
-    {
-        $container = new ServiceContainer();
-        $container->get(Fix\First::class);
-    }
-
-    /**
-     * @Revs(1000)
-     * @Iterations(5)
-     */
-    public function benchGetWithSingleton()
-    {
-        $this->container->get(Fix\First::class);
+        $this->container->get((string) array_pop($this->classnames));
     }
 }
